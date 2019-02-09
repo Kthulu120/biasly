4494d99b58c1f619ff838263770a5aaeb4ca3b11
troyleesmith <troysmith@schedulenav.com>
2019-02-09 12:12:22 -0800

    init

--- src/js/popup/example.js
+++ src/js/popup/example.js
@@ -1,3 +1,13 @@
 export default function () {
-  alert("hello! (find me on src/js/popup/example.js)");
+  var message = document.querySelector('#test');
+
+  chrome.tabs.executeScript(null, {
+    file: "test.js"
+  }, function() {
+    // If you try and inject into an extensions page or the webstore/NTP you'll get an error
+    if (chrome.runtime.lastError) {
+      message.innerText = 'There was an error injecting script : \n' + chrome.runtime.lastError.message;
+    }
+  });
+
 };
