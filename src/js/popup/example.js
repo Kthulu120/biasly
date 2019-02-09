d1479675ffdd1e2a2032c2e7b7d0a2aa3ab9c2b4
troyleesmith <troysmith@schedulenav.com>
2019-02-09 13:38:11 -0800

    Twitter makes

--- src/js/popup/example.js
+++ src/js/popup/example.js
@@ -1,13 +1,30 @@
+function placeBiaslyIcon(doc){
+  let posts = document.getElementsByClassName("stream-item-footer")
+  console.log(posts)
+  posts.forEach(element => {
+    console.log(element);
+    element.insertAdjacentHTML('beforeend', "<img src=\"https://visualpharm.com/assets/402/Message-595b40b65ba036ed117d44c1.svg\"/>")
+  });
+}
 export default function () {
   var message = document.querySelector('#test');
 
-  chrome.tabs.executeScript(null, {
-    file: "test.js"
-  }, function() {
-    // If you try and inject into an extensions page or the webstore/NTP you'll get an error
-    if (chrome.runtime.lastError) {
-      message.innerText = 'There was an error injecting script : \n' + chrome.runtime.lastError.message;
-    }
-  });
+  chrome.tabs.executeScript({
+    code: '(' + function(params) {
+        console.log(document);
+        let posts = document.getElementsByClassName("stream-item-footer")
+        console.log(posts)
+        for(let i =0; i < posts.length; i++){
+          posts[i].insertAdjacentHTML('beforeend', "<img height=50px width=50px src=\"https://visualpharm.com/assets/402/Message-595b40b65ba036ed117d44c1.svg\"/>")
 
+        }
+        document.body.style.background = "green";
+        return {success: true, html: document.body.innerHTML};
+    } + ')(' + JSON.stringify() + ');'
+}, function(results) {
+    console.log(results);
+    console.log(document)
+    placeBiaslyIcon(document);
+
+});
 };
